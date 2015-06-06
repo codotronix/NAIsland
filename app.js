174342ed789ce07bd4563a86987b63ff8432e6ad
codotronix <codotronix@gmail.com>
2015-06-06 16:20:53 +0530

    some modifications for mobile lumia

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.post('/signup', function(req, res) {
 	res.send('Hey, <br/> Thanks for your interest... <br/>  I am wrtiting the code for validation now... <br/>  See you soon...<br/>    - Suman Barick');
 });
 
-app.listen(9090); console.log('Node Express Server running at
-localhost:9090');
+app.listen(9090);
+console.log('Node Express Server running at localhost:9090');
