bf3081d3a8f6029e4e23f350a08de0523fad40a5
codotronix <codotronix@gmail.com>
2015-06-06 15:03:19 +0530

    cleanup

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express'),
 	app = express();
 
 app.use(express.static((__dirname, 'public')));
-
-app.post('/signup', function(req, res){     res.send('Hey, <br/>
-Thanks for your interest... <br/>  I am wrtiting the code for
-validation now... <br/>  See you soon...<br/>    - Suman Barick'); });
+	
+app.post('/signup', function(req, res) {
+	res.send('Hey, <br/> Thanks for your interest... <br/>  I am wrtiting the code for validation now... <br/>  See you soon...<br/>    - Suman Barick');
+});
 
 app.listen(9090); console.log('Node Express Server running at
 localhost:9090');
