fb9c3cf8695a1c2c3aebe28cec0c8d6ef2ff10be
Suman Barick <codotronix@gmail.com>
2016-11-05 21:09:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express'),     
 	app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static((__dirname, 'public')));
 	
 app.post('/signup', function(req, res) {
 	res.send('Hey, <br/> Thanks for your interest... <br/>  I am wrtiting the code for validation now... <br/>  See you soon...<br/>    - Suman Barick');
 });
 
-app.listen(9090);
-console.log('Node Express Server running at localhost:9090');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
